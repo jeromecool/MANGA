28f4fd8f3cb721d377a8398d84a9720a058c7697
esnault <jeromedev@esnault.link>
2020-08-31 16:54:38 +0200

    modif-multi

--- api/router.js
+++ api/router.js
@@ -7,7 +7,10 @@ const express = require('express'),
 const homeController = require('./controllers/homeController'),
     articleController = require('./controllers/articleController'),
     contactController = require('./controllers/contactController'),
-    referenceController = require('./controllers/referenceController')
+    referenceController = require('./controllers/referenceController'),
+    adminController = require('./controllers/adminController'),
+    adminpartController = require('./controllers/adminpartController'),
+    userController = require('./controllers/userController')
 
 // Home
 router.route('/')
@@ -23,6 +26,18 @@ router.route('/contact')
 
 // reférence
 router.route('/reference')
-    .get(referenceController.get)
+.get(referenceController.get)
+
+// admin
+router.route('/admin')
+    .get(adminController.get)
+
+// adminpart
+router.route('/adminpart')
+    .get(adminpartController.get)
+
+// user
+router.route('/user')
+    .get(userController.get)
 
 module.exports = router;
